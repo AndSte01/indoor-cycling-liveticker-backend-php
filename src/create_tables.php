ed0c4ff26efb7f930bb288f1f8c8d7bd02fa9aa1
AndSte01 <steger.andreas.2707@gmail.com>
2022-04-24 22:18:02 +0200

    added comment containing error logging

--- src/create_tables.php
+++ src/create_tables.php
@@ -10,6 +10,10 @@
 use function db\connect;
 use function db\createTables;
 
+// Error logging
+// ini_set('display_errors', 1);
+// error_reporting(E_ALL);
+
 // add required database tools
 require_once("db/adapter/db_adapter_generic.php");
 
