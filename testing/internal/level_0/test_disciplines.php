f1096cc313d05123b39d599f4a700660e5c97fc3
AndSte01 <steger.andreas.2707@gmail.com>
2022-05-01 13:11:43 +0200

    fixed requires, die to printf(), added remove test

--- testing/internal/level_0/test_disciplines.php
+++ testing/internal/level_0/test_disciplines.php
@@ -16,8 +16,8 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 // embed required files
-require_once(dirname(__FILE__) . "/../../../db/adapter/db_adapter_discipline.php");
-require_once(dirname(__FILE__) . "/../../../db/adapter/db_adapter_generic.php");
+require_once(dirname(__FILE__) . "/../../../src/db/adapter/db_adapter_discipline.php");
+require_once(dirname(__FILE__) . "/../../../src/db/adapter/db_adapter_generic.php");
 
 // connect to database
 $db = adapterGeneric::connect();
@@ -27,17 +27,17 @@ header("Content-Type: application/json");
 
 // --- SEARCH FOR DISCIPLINES ---
 // search for all disciplines
-// die(json_encode(adapterDiscipline::search($db)));
+// printf(json_encode(adapterDiscipline::search($db)));
 
 // search for discipline by id
-// die(json_encode(adapterDiscipline::search($db, 1)));
+// printf(json_encode(adapterDiscipline::search($db, 1)));
 
 // search for discipline by competition
-// die(json_encode(adapterDiscipline::search($db, null, 1)));
+// printf(json_encode(adapterDiscipline::search($db, null, 1)));
 
 // search for discipline by timestamp
 // $timestamp = (new DateTime())->modify("-1 day");
-// die(json_encode(adapterDiscipline::search($db, null, null, $timestamp)));
+// printf(json_encode(adapterDiscipline::search($db, null, null, $timestamp)));
 
 // --- ADD DISCIPLINES TO DATABASE ---
 // $discipline1 = new discipline(null, null, 1, -1, "Test Disziplin 2", 0, true);
@@ -47,6 +47,9 @@ header("Content-Type: application/json");
 // $discipline2 = new discipline(null, null, 1, -1, "Test Disziplin 3", 0, true);
 // $discipline2->updateId($result[0]->{discipline::KEY_ID});
 // adapterDiscipline::edit($db, [$discipline2]);
+
+// --- REMOVE DISCIPLINES FORM DATABASE ---
+// adapterDiscipline::remove($db, [new discipline(3)], true);
 
 // disconnect form database
 $db->close();
